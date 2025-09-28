c8f4ffef541ce8cb4d585cff8fca74f8f2804ab5
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-09-28 23:46:00 +0600

    Connect to MongoDB Successfully

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response } from "express";
+// import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// import walletRouter from "./app/modules/wallet/wallet.routes";
+// import adminRouter from "./app/modules/admin/admin.routes";
+// import { errorHandler } from "./app/middlewares/globalErrorHandler";
+// import authRouter from "./app/modules/auth/auth.routes";
+// import transactionRouter from "./app/modules/transaction/transaction.routes";
+
+
+ const app = express();
+
+// Middlewares
+app.use(
+  cors({
+    // origin: 'http://localhost:5173',
+    origin: 'https://digital-wallet-client-beta.vercel.app',
+     credentials: true,
+   })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+// if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+// Routes
+// app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/wallet", walletRouter);
+// app.use("/api/v1/transactions", transactionRouter);
+// app.use("/api/v1/admin", adminRouter);
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to Wallet Management Website');
+});
+// Error handling
+// app.use(errorHandler);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,80 @@
+/* eslint-disable no-console */
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+// import { envVars } from "./app/config/env";
+import dotenv from "dotenv";
+
+
+
+let server: Server;
+
+dotenv.config();
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL!)
+
+        console.log("Successfully Connected to MongoDB!!");
+
+        server = app.listen(process.env.PORT, () => {
+            console.log(`Server is listening to port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+(async () => {
+    await startServer()
+     
+})()
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received... Server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal recieved... Server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+
+process.on("unhandledRejection", (err) => {
+    console.log("Unhandled Rejecttion detected... Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+process.on("uncaughtException", (err) => {
+    console.log("Uncaught Exception detected... Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
