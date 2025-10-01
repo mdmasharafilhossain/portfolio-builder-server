bb3a2c60cbcdef019ca28b5daa7dd785765ee8b7
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-10-01 19:05:49 +0600

    All Blogs Route Complete

--- src/modules/blog/blog.routes.ts
+++ src/modules/blog/blog.routes.ts
@@ -1,15 +1,22 @@
 // src/routes/blog.routes.ts
 import express from 'express';
 import { blogController } from './blog.controller';
-import { authenticateToken, verifyToken } from '../../middleware/auth';
+import { authenticateToken, requireAdmin, verifyToken } from '../../middleware/auth';
 
 
 
 const router = express.Router();
 
 
-
+router.get('/', blogController.getAllPublishedBlogs);
+router.use(authenticateToken, requireAdmin);
 router.post('/',authenticateToken,blogController.createBlog);
 
+router.put('/:id', blogController.updateBlog);
+router.delete('/:id', blogController.deleteBlog);
+
+// Admin only routes for management
+router.get('/admin/all', blogController.getAllBlogs);
+router.get('/admin/:id', blogController.getBlogById);
 
 export default router;
