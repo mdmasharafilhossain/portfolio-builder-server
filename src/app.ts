d4eabcd14ff2da7457c4a1db53d254916c6de67d
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-10-09 23:39:02 +0600

    bug fixed

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: 'http://localhost:3000',
-    origin: 'https://portfolio-builder-client-rosy.vercel.app/',
+    origin: 'https://portfolio-builder-client-rosy.vercel.app',
      credentials: true,
    })
 );
